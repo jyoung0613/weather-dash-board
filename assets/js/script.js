92c927c6923072969bd181026e2818e37d37debd
John Young <jwyoung0613@gmail.com>
2021-08-29 19:11:05 -0600

    added some more structure in the html file to use bulma

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,19 @@
 var siteTitleEl = document.querySelector("#site-title");
+var zipcodeInputEl = document.querySelector("#user-entry");
+var buttonClickEl = document.querySelector("#get-zip");
+var zipcodeContainerEl = document.querySelector("#zipcode-container")
 
+var buttonSubmitHandler = function(event) {
+    // prevent page from refreshing
+    event.preventDefault();
+
+    // get value from input element
+    var zipcode = zipcodeInputEl.nodeValue.trim();
+
+    if (zipcode) {
+        getUserZipCode(zipcode);
+
+        // clear old content
+        
+    }
+}
