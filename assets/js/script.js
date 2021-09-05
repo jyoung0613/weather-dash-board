fead960b182139caef6aebb36bbd3132fa7a68a8
John Young <jwyoung0613@gmail.com>
2021-09-05 11:57:42 -0600

    added the rest of the structure for current weather forcast and most of the js to run it I think

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,64 @@
-var cityInput = document.querySelector("#city-input");
-var cityBtn = document.querySelector("#search-btn");
-var cityNameEl = document.querySelector("#city-name");
+var cityInput = document.querySelector('#city-input');
+var cityBtn = document.querySelector('#search-btn');
+var cityNameEl = document.querySelector('#city-name');
 var cityArr = [];
-var apiKey = "8ca68beeae3b0a314bcd89138e8d3a1f"; // Enter API Key
-
+var apiKey = '8ca68beeae3b0a314bcd89138e8d3a1f'; // please enter API Key here
 
 var formHandler = function(event) {
-    // create city name for display
+    // formats city name
     var selectedCity = cityInput
-    .value.trim().toLowerCase().split("").map((s) => s.charAt(0).toUpperCase() + s.substring(1)).join("");
+        .value
+        .trim()
+        .toLowerCase()
+        .split(' ')
+        .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+        .join(' ');
 
     if (selectedCity) {
-        getDeets(selectedCity);
-        cityInput.value = "";
+        getCoords(selectedCity);
+        cityInput.value = '';
     } else {
-        alert("Enter a City!");
+        alert('Please enter a city!');
     };
-
 };
 
-// use "current weather api" to fetch lattitude and longitude
+// uses 'current weather api' to fetch latitude and longitude
 var getCoords = function(city) {
-    var currentForecastApi = "https://api.openweathermap.org/data/2.5/weather/?appid=8ca68beeae3b0a314bcd89138e8d3a1f";
-    fetch(currentForecastApi).then(function(response) {
+    var currentWeatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+
+    fetch(currentWeatherApi).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
-                var lon = data.coord["lon"];
-                var lat = data.coord["lat"];
+                var lon = data.coord['lon'];
+                var lat = data.coord['lat'];
                 getCityForecast(city, lon, lat);
 
-                // save the searched cities and refreshes rencent city list
-                if (document.querySelector(".city-list")) {
-                    document.querySelector(".city-list").remove();
+                // saves searched city and refreshes recent city list
+                if (document.querySelector('.city-list')) {
+                    document.querySelector('.city-list').remove();
                 }
 
                 saveCity(city);
                 loadCities();
             });
         } else {
-            alert("Error: ${response.statusText}")
+            alert(`Error: ${response.statusText}`)
         }
     })
     .catch(function(error) {
-        alert("Could not load weather");
+        alert('Unable to load weather.');
     })
 }
 
-// use latitude and logitude to fetch current weather and five-day forecast
+// uses latitude and longitude to fetch current weather and five-day forecast
 var getCityForecast = function(city, lon, lat) {
-    var oneCallApi = "https://api.openweathermap.org/data/2.5/onecall?lat=lat&lon=lon&units=imperial&exclude=minutely,hourly,alerts&appid=8ca68beeae3b0a314bcd89138e8d3a1f";
+    var oneCallApi = `https://api.openweathermap.org/data/2.5/onecall?lat=lat&lon=lon&units=imperial&exclude=minutely,hourly,alerts&appid=8ca68beeae3b0a314bcd89138e8d3a1f`;
     fetch(oneCallApi).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
 
-                // identify city name in forecast
-                cityNameEl.textContent = '${city} (${moment().format('M/D/YYYY')})';
+                // identifies city name in forecast
+                cityNameEl.textContent = `${city} (${moment().format("M/D/YYYY")})`; 
 
                 console.log(data)
 
@@ -64,5 +68,4 @@ var getCityForecast = function(city, lon, lat) {
         }
     })
 }
-
 
